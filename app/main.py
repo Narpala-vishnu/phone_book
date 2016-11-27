480468a1fef52bc0aea920a8edbe435a3b46fa13
vishnu <narpala.vishnu.vardhan@gmail.com>
2016-11-28 00:23:38 +0530

    Added functionalities to functions

--- app/main.py
+++ app/main.py
@@ -17,22 +17,49 @@ def create_phone_book():
 	"""
 	initial_dict = {'phone_book': []}
 	with open("phone_book.yml", "w") as f:
-		yaml.dump(initial_dict, f)
+		yaml.dump(initial_dict, f, default_flow_style=False)
+	print('phone book created successfully')
 
 def get_phone_book():
-	pass
+	"""
+	Reads phone_book.yml and creates phone_book dictionary
+	"""
+	with open("phone_book.yml", "r") as f:
+		phone_book = yaml.load(f)
+	return phone_book
 
 def ask_information():
-	pass
+	"""
+	ask user for dynamic input and creates new_record dict
+	"""
+	first_name = "vishnu"
+	last_name = "vardhan"
+	phone_number = "9666021667"
+	email = "vishnu.narpala@gmail.com"
+	new_record = {
+		"first_name": first_name,
+		"last_name": last_name,
+		"phone_number": phone_number,
+		"email": email,
+	}
+	return new_record
 
-def add_to_phonebook(new_record=None):
-	pass
+
+def add_to_phonebook(phone_book=None, new_record=None):
+	"""
+	This function takes previous phone_book dict and new_record
+	appends new_record to phone_book dict
+	writes updated phone_book dict to phone_book.yml
+	"""
+	phone_book['phone_book'].append(new_record)
+	with open("phone_book.yml", "w") as f:
+		yaml.dump(phone_book, f, default_flow_style=False)
 
 if __name__ == '__main__':
 	if not phone_book_exists():
 		create_phone_book()
 	else:
-		phone_book = get_phone_book()
 		new_record = ask_information()
-		add_to_phonebook(new_record)
+		phone_book = get_phone_book()
+		add_to_phonebook(phone_book, new_record)
 		print('Contact successfully Added')
